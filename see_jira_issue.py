bb5dae7f153855eeb8e396d0377a9720595c267f
JeanCarloMachado <jean.machado@getyourguide.com>
2022-02-25 11:25:37 +0100

    automatic commit

--- see_jira_issue.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-
-from grimoire.ask_question import AskQuestion
-import os
-issue= AskQuestion().ask("Give the issue id")
-os.system(f"$BROWSER https://getyourguide.atlassian.net/browse/{issue}")
