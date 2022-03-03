b13612d42b2b0a7180166dad4e157618c654d2d6
JeanCarloMachado <jean.machado@getyourguide.com>
2022-03-03 16:20:51 +0100

    automatic commit

--- change_branch.py
+++ change_branch.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
-
+import fire
 
 from grimoire.ask_question import AskQuestion
 import os
-branch = AskQuestion().ask("Give the branch name")
-os.system(f"git fetch ; git checkout {branch}")
+
+class Cli():
+    def ask(self):
+        branch = AskQuestion().ask("Give the branch name")
+        self.change(branch)
+
+    def change(self, branch):
+        os.system(f"git fetch ; git checkout {branch}")
+
+
+if __name__ == '__main__':
+    fire.Fire(Cli)
+
